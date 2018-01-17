d374ba5ebfe45bf8c720a95b894188350b54e2af
LeraB <kudravka13@gmail.com>
2018-01-18 00:41:14 +0200

    tasks 4,6,7,8 done

--- index.js
+++ index.js
@@ -80,3 +80,76 @@ function reverse(str) {
 reverse('');                     // ''
 reverse('abcdef');               // 'fedcba'
 
+
+function missing(arr) {
+    var result = [], el,
+        len = arr.length - 1;
+    arr.sort();
+    for (var i = 1; i <= arr[len]; i++) {
+        el = arr.includes(i);
+        if (el === false) {
+            result.push(i);
+        }
+
+    }
+    if (result.length === 0) {
+        result = undefined;
+    }
+    return result;
+}
+// missing - Takes an unsorted array of unique numbers (ie. no repeats) from 1 through some number n, and returns the missing number in the sequence (there are either no missing numbers, or exactly one missing number). Can you do it in O(N) time? Hint: There’s a clever formula you can use.
+missing([]);                         // undefined
+missing([1, 4, 3]);                  // 2
+missing([2, 3, 4]);                  // 1
+missing([5, 1, 4, 2]);               // 3
+missing([1, 2, 3, 4]);               // undefined
+
+
+function indexOf(arr, el) {
+    for (var i = 0; i <= arr.length - 1; i++) {
+        if (arr[i] === el) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+//indexOf - Implement the indexOf function for arrays.
+indexOf([1, 2, 3], 1);           // 0
+indexOf([1, 2, 3], 4);              // -1
+
+
+function isSorted(arr) {
+    if (arr.length > 1) {
+        for (var i = 0; i <= arr.length - 1; i++) {
+            if ((arr[i] < arr[i - 1]) && (arr[i] < arr[i + 1]) || (arr[i] > arr[i - 1]) && (arr[i] > arr[i + 1])) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+// isSorted - Returns true or false, indicating whether the given array of numbers is sorted.
+isSorted([]);                        // true
+isSorted([-Infinity, -5, 0, 3, 9]);  // true
+isSorted([3, 9, -3, 10]);            // false
+
+
+function isPalindrome(str) {
+    var strLen = str.length,
+        result = true,
+        lowerStr = str.toLowerCase();
+    for (var i = 0; i < strLen; i++) {
+        if (lowerStr[i] !== lowerStr[strLen - 1 - i]) {
+            result = false;
+        }
+    }
+    return result;
+}
+
+// isPalindrome - Return true or false indicating whether the given string is a plaindrone (case and space insensitive).
+isPalindrome('');                                // true
+isPalindrome('abcdcba');                         // true
+isPalindrome('abcd');                            // false
+isPalindrome('A man a plan a canal Panama');     // true
